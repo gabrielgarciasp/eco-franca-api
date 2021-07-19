eced5052b3dd09ab54d01f329993d6803e9ac78b
Gabriel Garcia <gabrielgarciasp@gmail.com>
2021-07-19 13:25:01 -0300

    Removido a obrigatoriedade do endereço

--- src/schemas/Occurrence/createOccurrenceSchema.ts
+++ src/schemas/Occurrence/createOccurrenceSchema.ts
@@ -3,9 +3,9 @@ import * as Yup from 'yup'
 const schema = Yup.object().shape({
     category: Yup.string().required(),
     description: Yup.string().required(),
-    address: Yup.string().required(),
-    number: Yup.string().required(),
-    district: Yup.string().required(),
+    address: Yup.string(),
+    number: Yup.string(),
+    district: Yup.string(),
     reference: Yup.string(),
     latitude: Yup.string(),
     longitude: Yup.string(),
